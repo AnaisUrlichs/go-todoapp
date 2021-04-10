06998ddd9073fb825e701e02a8fb61fe2994550b
AnaisUrlichs <urlichsanais@gmail.com>
2021-04-10 13:58:32 +0200

    Changes to packages

--- backend/go.mod
+++ backend/go.mod
@@ -3,14 +3,13 @@ module github.com/AnaisUrlichs/go-todoapp
 go 1.16
 
 require (
-	github.com/armon/consul-api v0.0.0-20180202201655-eb2c6b5be1b6 // indirect
-	github.com/go-sql-driver/mysql v1.6.0 // indirect
-	github.com/gorilla/mux v1.8.0 // indirect
-	github.com/jinzhu/gorm v1.9.16 // indirect
-	github.com/rs/cors v1.7.0 // indirect
-	github.com/sirupsen/logrus v1.8.1 // indirect
-	github.com/ugorji/go v1.1.4 // indirect
-	github.com/xordataexchange/crypt v0.0.3-0.20170626215501-b2862e3d0a77 // indirect
-	gorm.io/driver/mysql v1.0.5 // indirect
-	gorm.io/gorm v1.21.7 // indirect
+	github.com/go-sql-driver/mysql v1.6.0
+	github.com/gorilla/mux v1.8.0
+	github.com/jinzhu/gorm v1.9.16
+	github.com/jinzhu/now v1.1.2 // indirect
+	github.com/rs/cors v1.7.0
+	github.com/sirupsen/logrus v1.8.1
+	github.com/stretchr/testify v1.4.0 // indirect
+	golang.org/x/sys v0.0.0-20210403161142-5e06dd20ab57 // indirect
+	gopkg.in/yaml.v2 v2.4.0 // indirect
 )
--- backend/todolist.go
+++ backend/todolist.go
@@ -1,19 +1,19 @@
 package main
 
 import (
+	"encoding/json"
 	"io"
 	"net/http"
-	"github.com/gorilla/mux"
-	log "github.com/sirupsen/logrus"
-	_ "github.com/go-sql-driver/mysql"
-	_ "github.com/jinzhu/gorm/dialects/mysql"
-	"encoding/json"
 	"strconv"
-	"github.com/rs/cors"
+	_ "github.com/go-sql-driver/mysql"
+	"github.com/gorilla/mux"
 	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql"
+	"github.com/rs/cors"
+	log "github.com/sirupsen/logrus"
 )
 
-var db, _ = gorm.Open("mysql", env.Username + ":" + env.Password + "@" + env.Host + "/" + env.Name + "?charset=utf8&parseTime=True&loc=Local")
+var db, _ = gorm.Open("mysql", env.Username+":"+env.Password+"@"+env.Host+"/"+env.Name+"?charset=utf8&parseTime=True&loc=Local")
 
 func init() {
 	log.SetFormatter(&log.TextFormatter{})
@@ -28,18 +28,18 @@ func UpdateItem(w http.ResponseWriter, r *http.Request) {
 	// Test if the TodoItem exist in DB
 	err := GetItemByID(id)
 	if err == false {
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
-		} else {
-			completed, _ := strconv.ParseBool(r.FormValue("completed"))
-			log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
-			todo := &TodoItemModel{}
-			db.First(&todo, id)
-			todo.Completed = completed
-			db.Save(&todo)
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, `{"updated": true}`)
-		}
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": false, "error": "Record Not Found"}`)
+	} else {
+		completed, _ := strconv.ParseBool(r.FormValue("completed"))
+		log.WithFields(log.Fields{"Id": id, "Completed": completed}).Info("Updating TodoItem")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		todo.Completed = completed
+		db.Save(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"updated": true}`)
+	}
 }
 
 func DeleteItem(w http.ResponseWriter, r *http.Request) {
@@ -50,24 +50,24 @@ func DeleteItem(w http.ResponseWriter, r *http.Request) {
 	// Test if the TodoItem exist in DB
 	err := GetItemByID(id)
 	if err == false {
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, `{"deleted": false, "error": "Record Not Found"}`)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"deleted": false, "error": "Record Not Found"}`)
 	} else {
-			log.WithFields(log.Fields{"Id": id}).Info("Deleting TodoItem")
-			todo := &TodoItemModel{}
-			db.First(&todo, id)
-			db.Delete(&todo)
-			w.Header().Set("Content-Type", "application/json")
-			io.WriteString(w, `{"deleted": true}`)
+		log.WithFields(log.Fields{"Id": id}).Info("Deleting TodoItem")
+		todo := &TodoItemModel{}
+		db.First(&todo, id)
+		db.Delete(&todo)
+		w.Header().Set("Content-Type", "application/json")
+		io.WriteString(w, `{"deleted": true}`)
 	}
 }
 
 func GetItemByID(Id int) bool {
 	todo := &TodoItemModel{}
 	result := db.First(&todo, Id)
-	if result.Error != nil{
-			log.Warn("TodoItem not found in database")
-			return false
+	if result.Error != nil {
+		log.Warn("TodoItem not found in database")
+		return false
 	}
 	return true
 }
@@ -85,7 +85,7 @@ func GetIncompleteItems(w http.ResponseWriter, r *http.Request) {
 	w.Header().Set("Content-Type", "application/json")
 	json.NewEncoder(w).Encode(IncompleteTodoItems)
 }
-	
+
 func GetTodoItems(completed bool) interface{} {
 	var todos []TodoItemModel
 	TodoItems := db.Where("completed = ?", completed).Find(&todos).Value
@@ -96,16 +96,16 @@ func CreateItem(w http.ResponseWriter, r *http.Request) {
 	description := r.FormValue("description")
 	log.WithFields(log.Fields{"description": description}).Info("Add new TodoItem, Saving to the database")
 	todo := &TodoItemModel{Description: description, Completed: false}
-    db.Create(&todo)
-    result := db.Last(&todo)
-    w.Header().Set("Content-Type", "application/json")
-    json.NewEncoder(w).Encode(result.Value)
+	db.Create(&todo)
+	result := db.Last(&todo)
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(result.Value)
 }
 
 type TodoItemModel struct {
-    Id int `gorm:"primary_key"`
-    Description string
-    Completed bool
+	Id          int `gorm:"primary_key"`
+	Description string
+	Completed   bool
 }
 
 func Healthz(w http.ResponseWriter, r *http.Request) {
@@ -116,7 +116,7 @@ func Healthz(w http.ResponseWriter, r *http.Request) {
 
 func main() {
 	db.Debug().DropTableIfExists(&TodoItemModel{})
-    db.Debug().AutoMigrate(&TodoItemModel{})
+	db.Debug().AutoMigrate(&TodoItemModel{})
 
 	log.Info("Starting todo list API server")
 	router := mux.NewRouter()
@@ -126,7 +126,7 @@ func main() {
 	router.HandleFunc("/todo", CreateItem).Methods("POST")
 	router.HandleFunc("/todo/{id}", UpdateItem).Methods("POST")
 	router.HandleFunc("/todo/{id}", DeleteItem).Methods("DELETE")
-	
+
 	handler := cors.New(cors.Options{
 		AllowedMethods: []string{"GET", "POST", "DELETE", "PATCH", "OPTIONS"},
 	}).Handler(router)
